e6fb90db67967e47bf86011cea4f22815e07f1c4
bored-guy <2232652509@qq.com>
2022-05-31 20:42:00 +0800

    finish a new page which can give an effect of auto typing

---
+++ statics/js/windows.js
@@ -0,0 +1,144 @@
+let message = [
+    ["关于“双生子佯谬”", "h1"],
+    ["一．引言", "h2"],
+    ["双生子佯谬是狭义相对论范畴内产生的一个佯谬，它作为对狭义相对论的质疑而被提出。但它并非真正意义上的悖论，而只是来源于对狭义相对论适用范围的错误理解。随着广义相对论的建立，这一矛盾也自然地迎刃而解。", "p"],
+    ["双生子佯谬的叙述如下：", "p"],
+    ["假设有一对双生子，哥哥在地球上，而弟弟乘上接近光速的宇宙飞船飞出一定距离再飞回，直到与哥哥相遇。根据钟慢效应，哥哥应当认为弟弟在航行中经历的时间比哥哥短，兄弟俩见面后弟弟比哥哥年轻；但如果我们以弟弟为参考系，飞出又飞回的人变成了哥哥，这样一来弟弟似乎应该认为哥哥比弟弟年轻。这似乎产生了不可调和的矛盾。", "p"],
+    ["根据狭义相对论的结果，如果哥哥和弟弟的相对速度接近光速，那么比较“哥哥与弟弟的年龄“并没有意义。因为两个参考系间对”同时“的概念不可能达成一致意见。但当弟弟减速进入哥哥的参考系，两人之间的年龄就不可避免地要相互比较。这一突出矛盾也就无法再回避。", "p"],
+    ["二．讨论", "h2"],
+    ["学术界对双生子佯谬的解释存在一定争议。以爱因斯坦为代表的一些科学家认为佯谬超出了狭义相对论的范围，需要用广义相对论解决；有人则认为这一悖论可以在狭义相对论范畴内解决。在国内这一话题并未成为研究热点，但仍有一些学者进行讨论。以河北化工学院李果昌和张立根，华南师范大学刘智炜，王笑君为代表的一部分学者在“地球本征系“”远离地球的参考系“和”回到地球的参考系“三个参考系中分别讨论问题，并自称解决了悖论；然而在这一讨论过程中”飞船本征系“并未出现，对于“飞船本征系”区分于“地球本征系”的特征也未作明确说明，所以不能认为解决了悖论。吕嫣等人在论文中借助两个速度相反的参考系K’，K”合成来近似计算飞船本征系的本征时，具有一定价值。", "p"],
+    ["这一佯谬的逻辑谬误出现在哪里呢？谬误在于，我们不经思考地认为弟弟的参考系与哥哥的参考系是等价的。但这一前提本身值得怀疑。要思考这一问题，我们需要回到狭义相对论的出发点——相对性原理。相对性原理指出：“一切惯性参考系都是等价的。“但是如何区分“惯性参考系”和非惯性参考系“呢？正如那个科普书籍中的经典例子：我们日常生活中认为地球是惯性系；但如果我们以太阳系的尺度观测，就会认为地球围绕太阳旋转，太阳才是惯性系；若是把视角再拉大到银河系尺度，我们又会认为银心黑洞是惯性系等等。并不存在一个特殊的参考系可以自命为唯一的”惯性系“。由此不难得到一个更强的等效原理：”一切参考系都是等价的“。", "p"],
+    ["\n", "\n"],
+    ["爱因斯坦假设，惯性质量和引力质量是严格相等的。他进一步提出，处于一架封闭的飞船中的人，如果处于失重状态，那么他无从分辨自己究竟是在无重力影响的外太空还是和飞船一同在重力场中做自由落体运动。同样，如果感受到“重力”，他也无从分辨自己究竟是静止于重力场中还是在空间中做加速运动。简而言之，引力场和加速度是等效的，即所谓“等效原理”。只有引入引力场（即广义相对论指出的时空弯曲），不同的参考系间才是等价的。1890年匈牙利物理学家厄缶通过转矩实验，证明在10-8精度范围内惯性质量同引力质量相等，佐证了爱因斯坦假设的正确性。", "p"],
+    ["这也就意味着，当我们将参考系从地球系转到飞船系时，我们需要在飞船系中引入一个引力场，而这个引力场引起了飞船参考系的时间变慢。因此在飞船参考系中应用狭义相对论计算，得到哥哥比弟弟年轻的结论是不合理的。", "p"],
+    ["而在这段加速过程中时间膨胀的大小可以计算。我们在狭义相对论框架下可以推算其结果：", "p"],
+    ["statics/img/word/passage/passage1/image1.png", "img"],
+    ["如图,P1是地球，P2是目标星球，O是地球本征系S的原点，O‘是以飞船最大速度v相对S系沿正方向运动的惯性系S’的原点，S’与S系间进行过原点校准。认为飞船由地球起飞后经过可以忽略的很短一段加速时间进入O’系。设v=0.8c，S1S2=8l.y.", "p"],
+    ["在O系中，飞船到达S2时的时空坐标：", "p"],
+    ["statics/img/word/passage/passage1/image2.png", "img"],
+    ["变化至O’参考系：", "p"],
+    ["statics/img/word/passage/passage1/image3.png", "img"],
+    ["接下来飞船减速直至速度方向反向，大小达到v。注意，加速时间在飞船本征系中可忽略，而对于地球本征系，这部分时间差不可忽略。事实上，这部分正是广义相对论中“时间膨胀“效应的结果。", "p"],
+    ["引入参考系S”，相对S系以速度v向x轴负方向运动。注意，S”系没有与S系做过原点校正，需要引入一组待定常数。", "p"],
+    ["\n", "\n"],
+    ["statics/img/word/passage/passage1/image4.png", "img"],
+    ["忽略加速时间， S”系中“飞船速度发生突变”这一事件的坐标是（0，6y）。代入上式可以解得", "p"],
+    ["statics/img/word/passage/passage1/image5.png", "img"],
+    ["计算开始减速时，S’系中地球的时空坐标", "p"],
+    ["statics/img/word/passage/passage1/image6.png", "img"],
+    ["而减速完成时，S”系中地球的时空坐标", "p"],
+    ["statics/img/word/passage/passage1/image7.png", "img"],
+    ["需要指出的是，同时具有相对性，在S’系眼中“P1和P2同时发生”的事件在S系眼中并非同时发生。", "p"],
+    ["将两组坐标变换至地球本征系:", "p"],
+    ["statics/img/word/passage/passage1/image8.png", "img"],
+    ["这里Δt有特殊的意义。这意味着，尽管我们已经忽略在飞船本征系内的变速时间，在地球本征系内这一段变速时间仍然不可忽略。在弟弟这一观察者看来，在加速过程中哥哥的时间以极快的速度流逝，在短短的加速过程中钟表指针转过了12.8年的间隔。这实质上就是广义相对论预言的时间膨胀效应。利用广义相对论的计算结果与此完全一致。", "p"],
+    ["不难计算弟弟眼中哥哥所经历的时间：", "p"],
+    ["statics/img/word/passage/passage1/image9.png", "img"],
+    ["\n", "\n"],
+    ["这与哥哥在自己参考系中的计算相同。这并不是巧合，借助代数运算不难证明这两者始终相等。证明过程在文献[3]中已有详细论述，这里不再赘述。","p"],
+    ["这一部分计算局限在狭义相对论范畴内。我们使用两个惯性系来近似地摹画飞船本征系这一非惯性系的运动。在狭义相对论范畴中我们不能准确地说明哥哥参考系和弟弟参考系为何不等价，没有从真正意义上“弟弟的参考系”出发描述这段相对运动的过程。直接忽略加速过程这一近似是否合理也尚待考虑。对于双生子佯谬真正圆满的解释仍然要借助广义相对论进行。","p"],
+    ["三．结论","h2"],
+    ["\t一系列讨论证明，双生子佯谬并不是真正的悖论，而只是对狭义相对论的不当应用。借助广义相对论可以精确描述这一物理情景，而即使在狭义相对论范围内讨论，在合理的近似下也可以得到正确的结论。","p"],
+
+]
+let times = 0
+let pages = 0
+let current_id = -1
+let new_message = []
+for (let i = 0; i < message.length; i++) {
+    let messageKey = message[i]
+    if (messageKey[1] == "img" || messageKey[1] == "\n") {
+        new_message.push(messageKey)
+    } else {
+        for (let j = 0; j < messageKey[0].length; j++) {
+            let messageKeyKey = messageKey[0][j]
+            new_message.push([messageKeyKey, messageKey[1], i])
+        }
+    }
+
+}
+console.log(new_message)
+
+function change_the_height() {
+    let height = $(window).height()
+    let width = $(window).width()
+    let body_height = height - 30 - width / 1920 * 162 - 20 - width / 1920 * 50 - 2
+    $("#body").height(body_height)
+    let time_height = width/1920 * 50
+    let time_width = width/1920 * 90
+    $("#time").height(time_height)
+    $("#time").width(time_width)
+    $("#time").offset({top:height-time_height,left:width-width/1920 * 70-time_width})
+    $("#time").css("font-size", time_height/2*0.7)
+
+
+}
+
+function type() {
+    let the_message = new_message[times]
+    if (the_message[1] == "img") {
+        $("#" + pages.toString()).append("<img src='" + the_message[0] + "' style='text-align: center'>")
+    } else if (the_message[1] == "\n") {
+        pages += 1
+        $("#body").append("<div class=\"page\">\n" +
+            "        <div class=\"inside\">\n" +
+            "            <div class=\"ininside\"  id='" + pages.toString() + "'>\n" +
+            "            </div>\n" +
+            "        </div>\n" +
+            "    </div>")
+    } else {
+        if (current_id != the_message[2]) {
+            $("#" + pages.toString()).append("<" + the_message[1] + " id=" + "'" + the_message[2].toString() + "_'" + ">" + "&emsp;" + the_message[0] + "</" + the_message[1] + ">")
+        } else {
+            $("#" + the_message[2].toString() + "_").append(the_message[0])
+        }
+        current_id = the_message[2]
+    }
+
+    times += 1
+
+}
+function checkTime(i){
+    if(i<10) return "0"+i;
+    return i;
+}
+function change_time(){
+    let time=new Date();
+    let year=time.getFullYear();
+    let month=time.getMonth()+1;
+    let day=time.getDate();
+
+    let hour=checkTime(time.getHours());
+    let minute=checkTime(time.getMinutes());
+    $("#time_line_one").empty()
+    $("#time_line_one").append(hour+":"+minute)
+    $("#time_line_two").empty()
+    $("#time_line_two").append(year+"/"+month+"/"+day)
+
+}
+
+$(function () {
+    change_time()
+    change_the_height()
+    $("#body").empty()
+    $("#body").append("<div class=\"page\">\n" +
+        "        <div class=\"inside\">\n" +
+        "            <div class=\"ininside\"  id='0'>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>")
+    $(window).resize(function () {
+        change_the_height()
+    })
+    $(window).keypress(function () {
+        type()
+        $("#word_info").empty()
+        $("#word_info").append(" <p style=\"display: inline;line-height: 1px;float: left;transform: translate(0px,9px)\" >&emsp;第"+(pages+1).toString()+"页, 共"+(pages+1).toString()+"页&emsp; "+($("#body").text().length-50).toString()+"个字&emsp;</p>")
+
+
+        }
+    )
+    setInterval(change_time,1000*60);   //setInterval(fn,i) 定时器，每隔i秒执行fn
+
+})
+
